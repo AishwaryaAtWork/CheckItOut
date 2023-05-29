d4763b6665ff38687bcb9e7d34fe4a93e7ee1cbf
Aishwarya Pathak <pathakaishwarya54@gmail.com>
2023-05-30 00:06:15 +0530

    Basic template ready

--- base/views.py
+++ base/views.py
@@ -1,6 +1,14 @@
 from django.shortcuts import render
-from django.http import HttpResponse
+from django.views.generic.list import ListView
+from django.views.generic.detail import DetailView
+from .models import Task
 
 # Create your views here.
-def taskList(request):
-    return HttpResponse('To do list')
+class TaskList(ListView):
+    model = Task 
+    context_object_name = 'tasks'
+
+class TaskDetail(DetailView):
+    model = Task
+    context_object_name = 'task'
+    template_name = 'base/task.html'
