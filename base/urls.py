b8e53257fd370b4285e688c1937be3e30bb92c16
Aishwarya Pathak <pathakaishwarya54@gmail.com>
2023-05-31 00:01:16 +0530

    Custom login functionality added

--- base/urls.py
+++ base/urls.py
@@ -1,7 +1,10 @@
 from django.urls import path
-from .views import TaskList, TaskDetail, TaskCreate, TaskUpdate, TaskDelete
+from .views import TaskList, TaskDetail, TaskCreate, TaskUpdate, TaskDelete, CustomLogin
+from django.contrib.auth.views import LogoutView
 
 urlpatterns = [
+    path('login/',CustomLogin.as_view(),name='login'),
+    path('logout/',LogoutView.as_view(next_page='login'),name='logout'),
     path('',TaskList.as_view(),name='tasks'),
     path('task/<int:pk>/',TaskDetail.as_view(),name='task'),
     path('task-create/',TaskCreate.as_view(),name='task-create'),
